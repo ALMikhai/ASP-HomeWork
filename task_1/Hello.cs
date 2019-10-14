15f4c8442e27321d8a8379fd4042629bfab465a3
Alexander Mikhailenko <molotok539@gmail.com>
2019-10-15 03:17:54 +1000

    Add task_1 (Hello world).

---
+++ task_1/Hello.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task_1
+{
+    static class Hello
+    {
+        public static void Print()
+        {
+            Console.WriteLine("Hello world!!!");
+        }
+    }
+}
