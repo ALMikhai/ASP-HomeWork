60268178c60a29c7d2c38b67127b5b289ef9d702
Alexander Mikhailenko <molotok539@gmail.com>
2019-10-16 00:53:35 +1000

    Task_1 complete.

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -6,23 +6,23 @@ namespace task_1
     {
         static void Main(string[] args)
         {
-            //Hello.Print(); // 1 {}
+            Hello.Print(); // 1 {}
 
-            //Math.mathListener(); // 2 {}
+            Math.mathListener(); // 2 {}
 
-            //Array array = new Array(10, 0, 100); // 3{
-            //array.Print(); //}
+            Array array = new Array(10, 0, 100); // 3{
+            array.Print(); //}
 
-            //array.Sort(Array.SortTo.min); // 4{
-            //array.Print(); //}
+            array.Sort(Array.SortTo.min); // 4{
+            array.Print(); //}
 
-            //Console.WriteLine(Math.Formula_1(2)); // 5{
-            //Console.WriteLine(Math.Formula_2(2));
-            //Console.WriteLine(Math.Formula_3(2));
-            //Console.WriteLine(Math.Formula_4(2)); //}
+            Console.WriteLine(Math.Formula_1(2)); // 5{
+            Console.WriteLine(Math.Formula_2(2));
+            Console.WriteLine(Math.Formula_3(2));
+            Console.WriteLine(Math.Formula_4(2)); //}
 
-            Text text = new Text();
-            text.Info();
+            Text text = new Text(); // 6{
+            text.Info(); //}
         }
     }
 }
