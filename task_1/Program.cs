8d5fc0aea4261b9f93a683da3b29ac56e738a128
Alexander Mikhailenko <molotok539@gmail.com>
2019-10-16 00:44:54 +1000

    Add text parser.

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -6,16 +6,23 @@ namespace task_1
     {
         static void Main(string[] args)
         {
-            //Hello.Print();
-            //Math.mathListener();
-            //Array array = new Array(10, 0, 100);
-            //array.Print();
-            //array.Sort(Array.SortTo.min);
-            //array.Print();
-            Console.WriteLine(Math.Formula_1(2));
-            Console.WriteLine(Math.Formula_2(2));
-            Console.WriteLine(Math.Formula_3(2));
-            Console.WriteLine(Math.Formula_4(2));
+            //Hello.Print(); // 1 {}
+
+            //Math.mathListener(); // 2 {}
+
+            //Array array = new Array(10, 0, 100); // 3{
+            //array.Print(); //}
+
+            //array.Sort(Array.SortTo.min); // 4{
+            //array.Print(); //}
+
+            //Console.WriteLine(Math.Formula_1(2)); // 5{
+            //Console.WriteLine(Math.Formula_2(2));
+            //Console.WriteLine(Math.Formula_3(2));
+            //Console.WriteLine(Math.Formula_4(2)); //}
+
+            Text text = new Text();
+            text.Info();
         }
     }
 }
---
+++ task_1/Text.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task_1
+{
+    class Text
+    {
+        private string _text;
+
+        public Text()
+        {
+            Console.WriteLine("Write your text :");
+            _text = Console.ReadLine();
+        }
+
+        public void Info() // Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне
+        {
+            string word = "";
+            double wordsCount = 0;
+            double withOutSpace = _text.Length;
+            string lastWordsSumbols = "";
+
+            for(var i = 0; i < _text.Length; i++)
+            {
+                if (_text[i] == ' ')
+                    withOutSpace--;
+
+                if (Char.IsLetter(_text[i]))
+                {
+                    word += _text[i];
+                }
+                else
+                {
+                    if (word.Length > 1)
+                    {
+                        wordsCount++;
+                        lastWordsSumbols += word[word.Length - 1];
+                        word = "";
+                    }
+                }
+            }
+
+            if (word.Length > 1)
+            {
+                wordsCount++;
+                lastWordsSumbols += word[word.Length - 1];
+                word = "";
+            }
+
+            Console.WriteLine($"Количество слов: {wordsCount};");
+            Console.WriteLine($"Количество символов без пробелов: {withOutSpace};");
+            Console.WriteLine("Соотношение количество символов без пробелов к количеству слов: {0:N2};", System.Math.Round((withOutSpace / wordsCount), 2));
+            Console.WriteLine($"Слово из последних символов слов: '{lastWordsSumbols}'.");
+        }
+    }
+}
